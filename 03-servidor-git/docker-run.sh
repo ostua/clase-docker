9ee33d234bbd0653c1468dbf24339fb29df133a3
Enrique Ostúa <ostua@dte.us.es>
2021-10-01 18:34:55 +0200

    sin actualizar

--- 03-servidor-git/docker-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti -p 9418:9418 -p 8080:80 -p 8022:22 --name gitserver --hostname gitserver gitimage
--- 03-servidor-git/git-clone.sh
+++
@@ -1,3 +0,0 @@
-git clone git@172.17.0.2:/home/git/lab6 lab6-ssh
-git clone http://172.17.0.2/git/lab6 lab6-http
-git clone git://172.17.0.2/lab6 lab6-git
